56c905c3a044e3262bde924c14df0ea604b2b731
Betiel <betyuser15@gmail.com>
2021-01-20 18:18:36 -0600

    added files

---
+++ script.js
@@ -0,0 +1,147 @@
+var questions = [{
+    question: "Which African country was formerly known as Abyssinia?",
+    choices: ["Kenya", "Ethiopia", "South Africa", "Nigeria"],
+    correctAnswer: 1
+}, {
+    question: "In which European city would you find Orly airport?",
+    choices: ["Rome", "Berlin", "Paris", "Milan"],
+    correctAnswer: 2
+}, {
+    question: "Which country consumes the most chocolate per capita?",
+    choices: ["Italy", "Japan", "U.S.A", "Switzerland"],
+    correctAnswer: 3
+}, {
+    question: "Which of Shakespeare’s plays is the longest?",
+    choices: ["Romeo and Juliet", "Hamlet", "Othello", "King Lear"],
+    correctAnswer: 1
+}, {
+    question: "How many ribs are in a human body?",
+    choices: ["Ten", "Twenty", "Twenty-four", "Eighteen"],
+    correctAnswer: 2
+}];
+
+var currentQuestion = 0;
+var correctAnswers = 0;
+var quizOver = false;
+
+$(document).ready(function () {
+
+    // Display the first question
+    displayCurrentQuestion();
+    $(this).find(".quizMessage").hide();
+
+    // On clicking next, display the next question
+    $(this).find(".nextButton").on("click", function () {
+        if (!quizOver) {
+
+            value = $("input[type='radio']:checked").val();
+
+            if (value == undefined) {
+                $(document).find(".quizMessage").text("Please select an answer");
+                $(document).find(".quizMessage").show();
+            } else {
+                // TODO: Remove any message -> not sure if this is efficient to call this each time....
+                $(document).find(".quizMessage").hide();
+
+                if (value == questions[currentQuestion].correctAnswer) {
+                    correctAnswers++;
+                }
+
+                currentQuestion++; // Since we have already displayed the first question on DOM ready
+                if (currentQuestion < questions.length) {
+                    displayCurrentQuestion();
+                } else {
+                    displayScore();
+                    //                    $(document).find(".nextButton").toggle();
+                    //                    $(document).find(".playAgainButton").toggle();
+                    // Change the text in the next button to ask if user wants to play again
+                    $(document).find(".nextButton").text("Play Again?");
+                    quizOver = true;
+                }
+            }
+        } else { // quiz is over and clicked the next button (which now displays 'Play Again?'
+            quizOver = false;
+            $(document).find(".nextButton").text("Next Question");
+            resetQuiz();
+            displayCurrentQuestion();
+            hideScore();
+        }
+    });
+
+});
+
+
+// This displays the current question AND the choices
+function displayCurrentQuestion() {
+
+    console.log("In display current Question");
+
+    var question = questions[currentQuestion].question;
+    var questionClass = $(document).find(".quizContainer > .question");
+    var choiceList = $(document).find(".quizContainer > .choiceList");
+    var numChoices = questions[currentQuestion].choices.length;
+
+    // Set the questionClass text to the current question
+    $(questionClass).text(question);
+
+    // Remove all current <li> elements (if any)
+    $(choiceList).find("li").remove();
+
+    var choice;
+    for (i = 0; i < numChoices; i++) {
+        choice = questions[currentQuestion].choices[i];
+        $('<li><input type="radio" value=' + i + ' name="dynradio" />' + choice + '</li>').appendTo(choiceList);
+    }
+}
+
+function resetQuiz() {
+    currentQuestion = 0;
+    correctAnswers = 0;
+    hideScore();
+}
+
+function displayScore() {
+    $(document).find(".quizContainer > .result").text("You scored: " + correctAnswers + " out of: " + questions.length);
+    $(document).find(".quizContainer > .result").show();
+}
+
+function hideScore() {
+    $(document).find(".result").hide();
+}
+const startingMinutes = 1;
+let time = startingSeconds * 60;
+
+const countdownEl = document.getElementById("countdown");
+
+/*setInterval(updateCountdown, 100);
+
+
+function updateCountdown{
+    const minutes = Math.floor(time / 60);
+    let seconds = time % 60;
+
+    countdownEl.innerHTML = ${minutes}: ${seconds};
+    time--;
+}*/
+
+function startTimer(duration, display) {
+    var timer = duration, minutes, seconds;
+    setInterval(function () {
+        minutes = parseInt(timer / 60, 10);
+        seconds = parseInt(timer % 60, 10);
+
+        minutes = minutes < 10 ? "0" + minutes : minutes;
+        seconds = seconds < 10 ? "0" + seconds : seconds;
+
+        display.textContent = minutes + ":" + seconds;
+
+        if (--timer < 0) {
+            timer = duration;
+        }
+    }, 1000);
+}
+function myfunction () {
+    var givenTime = 60 * 0.5,
+        display = document.querySelector('#countdown');
+    startTimer(givenTime, display);
+};
